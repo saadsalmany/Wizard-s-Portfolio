d4fe3821255fd0f8c1ad22e3e6a3e0a69ecfb79e
Saad Salmany <mohdsaad88842@gmail.com>
2024-09-16 15:10:43 +0530

    completed

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('is-open');
   navMenu.classList.toggle('open');
 });
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const projectCards = document.querySelectorAll('.project-card');
+    
+    projectCards.forEach((card, index) => {
+      card.style.animationDelay = `${index * 0.2}s`;
+      card.classList.add('float-animation');
+    });
+  });
